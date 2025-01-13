0fa76aad43e248e990b8fa70fce773f914287f31
shukrullojondevnu <shukrullojon.devnu@gmail.com>
2025-01-13 14:07:04 +0500

    commited

--- dist/components/card/filled.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.filled = void 0;
-exports.filled = {
-    boxShadow: "var(--md-sys-elevation-level-0)",
-    backgroundColor: "var(--md-sys-color-surface-container-highest)",
-    "&:hover": { boxShadow: "var(--md-sys-elevation-level-1)" },
-};
